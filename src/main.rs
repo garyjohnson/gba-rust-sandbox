c6b4433383eb2e576d2e04d19a6c96a8cc7af9e1
Gary Johnson <gary@gjtt.com>
2021-11-28 13:49:26 -0500

    Clear old ball position

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn main() -> ! {
     //const WHITE: u16 = color(1, 1, 1);
     //const BLACK: u16 = color(0, 0, 0);
     const WHITE: u16 = 0x001F;
-    const BLACK: u16 = 0x03E0;
+    const BLACK: u16 = 0x0000;
     const WHITE_PAL: u32 = 1;
     const BLACK_PAL: u32 = 2;
 
@@ -57,16 +57,22 @@ fn main() -> ! {
     let max_y: i32 = display::HEIGHT;
     let paddle_y_bottom: i32 = max_y - 10;
     let paddle_y_top: i32 = paddle_y_bottom - PADDLE_HEIGHT;
-    let mut paddle_x: i32 = -1;
-    let paddle_new_x: i32 = max_x / 2;
+    let mut paddle_x: i32 = max_x / 2;
+    let mut paddle_new_x: i32 = paddle_x;
     let mut ball_x: i32 = max_x/2;
     let mut ball_y: i32 = max_y/2;
-    let mut ball_new_x: i32 = max_x/2;
-    let mut ball_new_y: i32 = max_y/2;
+    let mut ball_new_x: i32 = ball_x;
+    let mut ball_new_y: i32 = ball_y;
     let mut ball_vx: i32 = DEFAULT_VX;
     let mut ball_vy: i32 = DEFAULT_VY;
 
-    let draw_rect = |bmp: &mut display::bitmap4::Bitmap4, x_min: i32, y_min: i32, x_max: i32, y_max: i32, color_pal: u8| {
+    let draw_rect = |bmp: &mut display::bitmap4::Bitmap4, mut x_min: i32, mut y_min: i32, mut x_max: i32, mut y_max: i32, color_pal: u8| {
+        x_min = clamp(0, x_min, max_x);
+        x_max = clamp(0, x_max, max_x);
+
+        y_min = clamp(0, y_min, max_y);
+        y_max = clamp(0, y_max, max_y);
+
         for x_pos in x_min..x_max {
             for y_pos in y_min..y_max {
                 (*bmp).draw_point(x_pos, y_pos, color_pal);
@@ -81,11 +87,20 @@ fn main() -> ! {
 
     loop {
         vblank.wait_for_vblank();
+        
+        // CLEAR
+        //draw_rect(&mut bitmap, paddle_x-PADDLE_HALF_WIDTH,paddle_y_top,paddle_x+PADDLE_HALF_WIDTH,paddle_y_bottom, BLACK_PAL as u8);
+        draw_rect(&mut bitmap, ball_x-BALL_RADIUS, ball_y-BALL_RADIUS, ball_x+BALL_RADIUS, ball_y+BALL_RADIUS, BLACK_PAL as u8);
+
+        // COMMIT POSITIONS
+        paddle_x = paddle_new_x;
+        ball_x = ball_new_x;
+        ball_y = ball_new_y;
 
         //mouse_info(&info);
 
         // CALC POSITIONS
-        //paddle_new_x = min(max(info.pos.x, PADDLE_HALF_WIDTH), max_x-PADDLE_HALF_WIDTH);
+        paddle_new_x = paddle_x;//min(max(paddle_new_x, PADDLE_HALF_WIDTH), max_x-PADDLE_HALF_WIDTH);
 
         ball_new_x += ball_vx;
         ball_new_y += ball_vy;
@@ -119,16 +134,5 @@ fn main() -> ! {
 
         // CYCLE GRAPHICS PAGES
         bitmap.flip_page();
-
-        // CLEAR
-        //draw_rect(&mut bitmap, paddle_x-PADDLE_HALF_WIDTH,paddle_y_top,paddle_x+PADDLE_HALF_WIDTH,paddle_y_bottom, BLACK_PAL as u8);
-        //draw_rect(&mut bitmap, ball_x-BALL_RADIUS, ball_y-BALL_RADIUS, ball_x+BALL_RADIUS, ball_y+BALL_RADIUS, BLACK_PAL as u8);
-
-        // COMMIT POSITIONS
-        paddle_x = paddle_new_x;
-        ball_x = ball_new_x;
-        ball_y = ball_new_y;
-
-        mgba.print(format_args!("ball x: {} y: {} paddle x: {}", ball_x, ball_y, paddle_x), agb::mgba::DebugLevel::Debug).unwrap();
     }
 }
